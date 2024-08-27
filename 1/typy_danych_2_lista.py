621bdced3c1346542fe47fd0ccf35326204a8307
radek <rajkonkret660@gmail.com>
2024-08-27 09:51:58 +0200

    [2.2] update typy_danych_2_lista.py

--- 1/typy_danych_2_lista.py
+++ 1/typy_danych_2_lista.py
@@ -75,3 +75,60 @@ print(lista.pop(5))  # Ania
 print(lista)
 print(lista.pop(-2))  # Zenek
 print(lista.pop())  # usunie ostatni element z listy, Magda
+
+a = 1
+b = 3
+a = b
+print(a, b)  # 3 3
+b = 7
+print(a, b)  # 3 7
+
+lista_2 = lista  # a = b, kopiuje referencje (adres w pamieci)
+lista_copy = lista.copy()  # kopia elementów listy
+lista.clear()  # b = 7, clear() - czyszczenie listy
+print(lista_2, lista)  # [] [], zmienione obie listy
+print(lista_copy)  # ['Radek', 'Karol', 'Tomek']
+print(id(lista_2))  # 2895633060224
+print(id(lista))  # 2895633060224
+print(id(lista_copy))  # 2895636446976
+print(type(lista))
+
+liczby = [54, 999, 34, 22, 12.34, 687]
+print(liczby)  # [54, 999, 34, 22, 12.34, 687]
+print(type(liczby))  # <class 'list'>
+
+liczby.sort()
+print(liczby)  # [12.34, 22, 34, 54, 687, 999]
+
+liczby = [54, 999, 34, 22, 12.34, 687, "A"]
+print(ord("A"))  # kod znaku (ascii) 65
+# liczby.sort()  # TypeError: '<' not supported between instances of 'str' and 'int'
+
+lista_osob = ['radek', "ola", "lena", "agata"]
+lista_osob.sort()
+print(lista_osob)  # ['agata', 'lena', 'ola', 'radek']
+
+lista_osob.sort(reverse=True)
+print(lista_osob)  # ['radek', 'ola', 'lena', 'agata']
+
+lista_osob.reverse()  # odwracanie elemntow listy bez sortowania
+print(lista_osob)  # ['agata', 'lena', 'ola', 'radek']
+
+liczby[3] = 666
+print(liczby[0:3])
+print(liczby[-2])
+print(liczby)  # [54, 999, 34, 666, 12.34, 687, 'A']
+
+del liczby  # usunie listę z pamięci
+# print(liczby)  # NameError: name 'liczby' is not defined
+
+tekst = "Pyth on."
+lista1 = list(tekst)  # rozpakowanie sekwencji
+print(lista1)  # ['P', 'y', 't', 'h', ' ', 'o', 'n', '.']
+
+lista2 = [tekst]
+print(lista2)  # ['Pyth on.']
+
+krotka = tuple(lista_copy)  # tuple() - rzytowanie na krotke (tuple)
+print(type(krotka))  # <class 'tuple'>
+print(krotka)  # ('Radek', 'Karol', 'Tomek')
