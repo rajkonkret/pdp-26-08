bb4307c4e0ccfdf8fc28d929800206de23b7da68
radek <rajkonkret660@gmail.com>
2024-08-27 10:38:50 +0200

    [2.4] update typy_danych_3_tupla.py

--- 1/typy_danych_3_tupla.py
+++ 1/typy_danych_3_tupla.py
@@ -31,3 +31,31 @@ print(type(tupla_imiona))  # <class 'tuple'>
 
 print(tupla_imiona.index("Radek"))  # indeks 0
 print(tupla_imiona.count("Olek"))  # występuje 1 raz w tupli
+
+# rozpakowanie tupli (krotki)
+tup = 1, 2
+print(type(tup))  # <class 'tuple'>
+a, b = 1, 2
+print(a, b)
+a, b = tup
+print(a, b)  # 1 2
+tup_1 = 1, 2, 3
+# a,b = tup_1  # ValueError: too many values to unpack (expected 2)
+a, *b = tup_1  # * worek na pozostałe dane
+print(a, b)  # 1 [2, 3]
+
+# ('Radek', 'Tomek', 'Zenek', 'Olek', 'Robert', 'Michał') 6 elemntów
+# name1, name2, name3, name4
+name1, name2, name3, *name4 = tupla_imiona
+print(name1, name2, name3, name4)  # Radek Tomek Zenek ['Olek', 'Robert', 'Michał']
+# 'Radek',,'Robert', 'Michał'
+name1, *name2, name3, name4 = tupla_imiona
+print(name1, name2, name3, name4)  # Radek ['Tomek', 'Zenek', 'Olek'] Robert Michał
+
+# sortowanie krotki zwraca liste posortowaną
+print(sorted(tupla_imiona))  # ['Michał', 'Olek', 'Radek', 'Robert', 'Tomek', 'Zenek']
+print(tupla_imiona)  # ('Radek', 'Tomek', 'Zenek', 'Olek', 'Robert', 'Michał')
+
+lista = list(tupla_imiona)
+print(lista)  # ['Radek', 'Tomek', 'Zenek', 'Olek', 'Robert', 'Michał']
+print(type(lista))  # <class 'list'>
